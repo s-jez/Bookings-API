5e7f00c1f8d82c86dab5a3498d76bb3081fb03d0
s-jez <stanislawjez7@gmail.com>
2022-09-20 14:35:06 +0200

    Add messages to endpoints.

--- app.js
+++ app.js
@@ -5,17 +5,25 @@ app.get("/", (req, res) => {
   res.status(200).send("<h3>Hello World!</h3>");
 });
 
-app.get("/books", (req, res) => {
-  res.status(200).send("Got a GET Bookings []");
+app.get("/book", (req, res) => {
+  res.status(200).send("Got a GET bookings.");
 });
 
-app.post("/book", (req, res) => {});
+app.post("/book", (req, res) => {
+  res.status(200).send("Got a POST bookings.");
+});
 
-app.get("/book/{id}", (req, res) => {});
+app.get("/book/{id}", (req, res) => {
+  res.status(200).send("Got a GET via ID bookings.");
+});
 
-app.put("/book/{id}", (req, res) => {});
+app.put("/book/{id}", (req, res) => {
+  res.status(200).send("Got a PUT via ID bookings.");
+});
 
-app.delete("/book/{id}", (req, res) => {});
+app.delete("/book/{id}", (req, res) => {
+  res.status(200).send("Got a DELETE via ID bookings.");
+});
 
 app.listen(3000, () => {
   console.log(`Example app listening on port 3000`);
