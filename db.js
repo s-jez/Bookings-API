d8c83684dc40e7890935137c9ac80c1158e167fd
s-jez <stanislawjez7@gmail.com>
2022-09-23 20:27:47 +0200

    Add env variables.

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const mysql = require("pg");
 
-const DATABASE_HOST = "";
-const DATABASE_USER = "";
-const DATABASE_PASS = "";
-const DATABASE_NAME = "";
+const DATABASE_HOST = process.env.DATABASE_HOST;
+const DATABASE_USER = process.env.DATABASE_USER;
+const DATABASE_PASS = process.env.DATABASE_PASS;
+const DATABASE_NAME = process.env.DATABASE_NAME;
 
 const { Client } = require("pg");
 
