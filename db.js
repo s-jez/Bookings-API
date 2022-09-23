c65116fa47a1d83328b5e3cf08916b305cecc785
s-jez <stanislawjez7@gmail.com>
2022-09-23 21:13:21 +0200

    Query guests from database working..

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 const mysql = require("pg");
+require("dotenv").config();
 
 const DATABASE_HOST = process.env.DATABASE_HOST;
 const DATABASE_USER = process.env.DATABASE_USER;
 const DATABASE_PASS = process.env.DATABASE_PASS;
 const DATABASE_NAME = process.env.DATABASE_NAME;
+const DATABASE_PORT = process.env.DATABASE_PORT;
 
 const { Client } = require("pg");
 
@@ -13,10 +15,11 @@ const dbConnection = async () => {
     password: DATABASE_PASS,
     database: DATABASE_NAME,
     host: DATABASE_HOST,
+    port: DATABASE_PORT,
   });
   await client.connect();
   try {
-    console.log(await client.query("SELECT *"));
+    console.log(await client.query("SELECT * FROM guests;"));
   } finally {
     await client.end();
   }
