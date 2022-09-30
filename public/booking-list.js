d51453d36b008947d3e2188d528c4f65d6521886
s-jez <stanislawjez7@gmail.com>
2022-09-30 21:49:16 +0200

    Add console.log to show id.

--- public/booking-list.js
+++ public/booking-list.js
@@ -31,15 +31,18 @@ const loadBookings = () => {
       }
     });
 };
+
 const updateBooking = (id) => {
   fetch(`http://localhost:3000/book/${id}`, {
     method: "PUT",
   });
+  console.log("Current booking id: ", id);
 };
 const deleteBooking = (id) => {
   fetch(`http://localhost:3000/book/${id}`, {
     method: "DELETE",
   });
+  console.log("Current booking id: ", id);
 };
 
 loadBookings();
